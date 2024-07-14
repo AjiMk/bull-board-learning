6b9ad41f5247952bd3f779593de444e4279bb93d
Ajay Kumar M <ajaymeledath007@gmail.com>
2024-07-14 12:19:37 +0530

    Changes  - Worker setuped  - Add job route added  - setFormater() used for QueueAdapters  - fast-redact used to strip out sensitive informations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import bull from "bull";
-import { Queue } from "bullmq";
+import fastRedact from "fast-redact"
+import { Job, Queue, Worker } from "bullmq";
 import { createBullBoard } from "@bull-board/api";
 import { BullAdapter } from "@bull-board/api/bullAdapter";
 import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
@@ -8,21 +9,51 @@ import { ExpressAdapter } from "@bull-board/express";
 
 const app = express();
 
+const redisConnectionOption = {
+  port: 6379,
+  host: 'localhost',
+};
+
+const redact = fastRedact({
+  paths: ['headers.cookie', 'password', 'access_token']
+})
+
+
 /**
- * Create a new queue
+ * Worker
+ * 
+ */
+new Worker("FirstQueue", async(job: Job) => {
+  return 1;
+}, {
+  connection: redisConnectionOption
+});
+
+
+/**
+ * Queue
  *
  */
 const queue = new Queue("FirstQueue");
 
 /**
- * Create a new server adapter
+ * Server adapter
+ * 
  */
 const serverAdapter = new ExpressAdapter();
-
 serverAdapter.setBasePath("/admin/queues");
 
+/**
+ * Queue adapter
+ * 
+ */
+const queueAdapter = new BullMQAdapter(queue, {readOnlyMode: false, description:"First Queue"});
+queueAdapter.setFormatter("name", (job: Job)=> {return `#${job.name}`});
+queueAdapter.setFormatter("data", (data: any)=> redact(data))
+queueAdapter.setFormatter("returnValue", (data) => data)
+
 createBullBoard({
-  queues: [new BullMQAdapter(queue)],
+  queues: [queueAdapter],
   serverAdapter: serverAdapter,
   options: {
     uiConfig:{
@@ -35,6 +66,17 @@ createBullBoard({
 });
 
 app.use("/admin/queues", serverAdapter.getRouter());
+
+/**
+ * Adding job
+ */
+app.get("/add/:id", (req: Request, res: Response) => {
+  const jobName = `dummy_job_${req.params.id}`;
+
+  queue.add(jobName, {"id":req.params.id});
+
+  return res.json({status: "ok"});
+})
 
 app.get("/logout", (req, res) => {
   return res.end();
